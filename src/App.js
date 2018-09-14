cf3bb2ac15b4f6ad45227c5c0610ca31591478e5
Andy March <andymarch@A-Marchs-MBP.gv.shawcable.net>
2018-09-14 16:06:25 -0700

    inline_cart_render

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import StripeCheckout from "react-stripe-checkout"
 import './App.css';
 import {data} from './store.js';
 import uuid from 'uuid/v4'
-import {PUBLIC_KEY} from './PUBLIC_KEY.js'
+// import {PUBLIC_KEY} from './PUBLIC_KEY.js'
 
-// const PUBLIC_KEY = 345657
+const PUBLIC_KEY = 345657
 
 const {name,products} = JSON.parse(data)
 const images = {}
@@ -14,19 +14,8 @@ products.forEach(product=>{
   var imageName = product.image.text
   images[imageName]=require('./images/'+imageName)  
 })
-var id=0;
 
-const totals = (acc,cur) => acc + cur.price * cur.quantity
-const remove = (list,i) => list.slice(0,i).concat(list.slice(i+1))
 const u = name => name.replace(/\s/g, '');
-
-const Product = ({history, product: {name, image} }) => 
-    <div className="Product">
-      <img onClick={()=>{history.push('/'+u(name.text))}} className="Product-image"  src={images[image.text]}/>
-      <div className="Product-bar">
-        <div className="Product-name">{name.text}</div>
-      </div>
-    </div>
 
 const Home = ({ cart,history }) =>
   <div>
@@ -36,6 +25,14 @@ const Home = ({ cart,history }) =>
     </div>
     {products.map((pr,i)=><Product key={i} product={pr} history={history}/>)}
   </div>
+
+const Product = ({history, product: {name, image} }) => 
+    <div className="Product">
+      <img onClick={()=>{history.push('/'+u(name.text))}} className="Product-image"  src={images[image.text]}/>
+      <div className="Product-bar">
+        <div className="Product-name">{name.text}</div>
+      </div>
+    </div>
 
 const ProductPage = ({ ATC, history, product: {description,price,name,image} }) =>
     <div className="Product">
@@ -48,60 +45,23 @@ const ProductPage = ({ ATC, history, product: {description,price,name,image} }) 
       <div className="Product-description">{description.text}</div>
     </div>
 
-class Counter extends Component{
-  constructor(props){
-    super(props)
-    this.state={num:this.props.quantity}
-  }
-  editQuantity(name,quantity){
-    this.props.modifyCart(name,quantity)
-    this.setState({num:quantity})
-  }
-  render(){
-    const {name} = this.props
-    const {num} = this.state
-    return(
-      <div style={{display:'flex',flexDirection:'row'}}>
-        <div className='Cart-remove-x' onClick={()=>this.editQuantity(name,num+1)}>+</div>
-          <div>quantity : {this.state.num} </div>
-        <div className='Cart-remove-x' onClick={()=>this.editQuantity(name,num-1)}>-</div>
-      </div>
-    );
-  }
-}
-
-const CartLine = ({item:{name,image,price,quantity},modifyCart}) =>
-  <div className='Cart-line' key={id++}>
-    <img className='Cart-item-image' src={images[image]}/>
-    <div className='Cart-item-name'>{name}</div>
-    <Counter modifyCart={modifyCart} quantity={quantity} name={name} />
-    {/* <div className='Cart-remove-x' onClick={()=>modifyCart(name,quantity+1)}>+</div>
-    <div>quantity : {quantity} </div>
-    <div className='Cart-remove-x' onClick={()=>quantity>1 && modifyCart(name,quantity-1)}>-</div> */}
-    <div className='Cart-item-price'>${price}</div>
-    <div className='Cart-remove-x' onClick={()=>modifyCart(name,0)}>x</div>
+const CartIcon = ({cart,history}) => 
+  <div className='Cart-icon' onClick={()=>history.push('/cart')}>
+    {cart.reduce((acc,cur)=>acc+cur.quantity,0)}
   </div>
 
-const Cart = ({modifyCart,cart,history}) => 
-      <div className='Cart-container'>
-        <div className='Cart-back' onClick={()=>history.push('/')} >continue shopping</div>
-        <div className='Items-container'>
-          {cart.filter(p=>p.quantity>0).map((item,i) => <CartLine item={item} modifyCart={modifyCart}/>)}
-        </div>
-        <div className='Cart-footer'>
-          {/* <span className='Cart-footer-total'>TOTAL : ${getTotal(this)}</span> */}
-          <span className='Cart-footer-checkout' onClick={()=>history.push('/checkout')}>checkout</span>
-        </div>
-      </div>
+const Field = ({label,name,value,onChange}) =>
+  <p>
+    <label>
+      {label} <input type={'text'} name={name} value={value} onChange={onChange} />
+    </label>
+  </p>
 
 
 class App extends Component {
   constructor(props){
     super(props)
-    this.state={
-      cart:[]
-    }
-    // products.forEach(p=>this.cart[p.name.text]={name:p.name.text,image:p.image.text,price:p.price.text,quantity:0})
+    this.state={cart:[]}
   }
   render() {
     return (
@@ -110,36 +70,34 @@ class App extends Component {
           <div className="Container">
             <Switch>
               <Route exact path='/' render={p=> <Home {...p} cart={this.state.cart}/>} />
-              <Route path='/cart'     render={p=>{
-                return(
-                  <div className='Cart-container'>
-                    <Link to='/'><div className='Cart-back' >continue shopping</div></Link>
-                    <div className='Items-container'>
-                      {this.state.cart.map((item,i) => {
-                        const {name,price,image,quantity} = item
-                        return(
-                          <div className='Cart-line'>
-                            <img className='Cart-item-image' src={images[image.text]}/>
-                            <div className='Cart-item-name'>{name.text}</div>
-                            <div className='Cart-remove-x' onClick={()=>{this.modCart(i,quantity+1)}}>+</div>
-                            <div>quantity : {item.quantity} </div>
-                            <div className='Cart-remove-x' onClick={()=>{this.modCart(i,quantity-1)}}>-</div>
-                            <div className='Cart-item-price'>${price.text}</div>
-                            <div className='Cart-remove-x' onClick={()=>{this.RFC(i)}}>x</div>
-                          </div>
-                      )})}
-                    </div>
-                    <div className='Cart-footer'>
-                      <span className='Cart-footer-total'>TOTAL : ${this.getTotal()}</span>
-                      <Link to='/checkout'><span className='Cart-footer-checkout'>checkout</span></Link>
-                    </div>
-                  </div>
-                )}} />
-              
               <Route path='/checkout' render={p=> <Checkout ref={i=>this.checkout=i} {...p} />} />
               {products.map(pr=>
-                <Route key={id++} path={'/'+u(pr.name.text)} render={p=> <ProductPage {...p} ATC={this.ATC} product={pr} />} />)}
-              <Route render={p=> <Home {...p} cart={Object.values(this.cart)}/>} />
+                <Route path={'/'+u(pr.name.text)} render={p=><ProductPage {...p} ATC={this.ATC} product={pr}/>}/>)}
+              <Route path='/cart' render={p=>
+                <div className='Cart-container'>
+                  <Link to='/'><div className='Cart-back' >continue shopping</div></Link>
+                  <div className='Items-container'>
+                    {this.state.cart.map((item,i) => {
+                      const {name,price,image,quantity} = item
+                      return(
+                        <div className='Cart-line'>
+                          <img className='Cart-item-image' src={images[image.text]}/>
+                          <div className='Cart-item-name'>{name.text}</div>
+                          <div className='Cart-remove-x' onClick={()=>{this.modCart(i,quantity+1)}}>+</div>
+                          <div>quantity : {item.quantity} </div>
+                          <div className='Cart-remove-x' onClick={()=>{this.modCart(i,quantity-1)}}>-</div>
+                          <div className='Cart-item-price'>${price.text}</div>
+                          <div className='Cart-remove-x' onClick={()=>{this.RFC(i)}}>x</div>
+                        </div>
+                    )})}
+                  </div>
+                  <div className='Cart-footer'>
+                    <span className='Cart-footer-total'>TOTAL : ${this.getTotal()}</span>
+                    <Link to='/checkout'><span className='Cart-footer-checkout'>checkout</span></Link>
+                  </div>
+                </div>
+              }/>
+  <Route render={p=> <Home {...p} cart={Object.values(this.cart)}/>} />
             </Switch>
           </div>
         </div>
@@ -169,11 +127,6 @@ class App extends Component {
   }
 }
 
-const CartIcon = ({cart,history}) => 
-  <div className='Cart-icon' onClick={()=>history.push('/cart')}>
-    {cart.reduce((acc,cur)=>acc+cur.quantity,0)}
-  </div>
-
 const fields = ['Name','Street Address','City', 'ZIP code / Postal Code', 'Country']
 
 class Checkout extends Component {
@@ -187,8 +140,8 @@ class Checkout extends Component {
     const userData = userDataStrings.join('\n')
     const tokenString = JSON.stringify(token,null,3).replace(/[^\w\s:_@.-]/g,'')
     return`
-${userData}
-stripe payment meta-data:${tokenString}`
+  ${userData}
+  stripe payment meta-data:${tokenString}`
   }
   onToken = token => {
     const data = {
@@ -226,7 +179,6 @@ stripe payment meta-data:${tokenString}`
           )})}
         </form>
         <StripeCheckout token={this.onToken} stripeKey={PUBLIC_KEY}/>      
-        {/* {Object.keys(this.state).map(s=><p>{s} : {this.state[s]}  </p>)} */}
       </div>
     )
   }
@@ -246,12 +198,5 @@ stripe payment meta-data:${tokenString}`
   };
   handleChange = e => this.setState({ [e.target.name]: e.target.value });
 }
-
-const Field = ({label,name,value,onChange}) =>
-  <p>
-    <label>
-      {label} <input type={'text'} name={name} value={value} onChange={onChange} />
-    </label>
-  </p>
 
 export default App;
